da11954762b506af0a5eac74c40c0825a81504cc
gharjare <gharjarechetan@gmail.com>
2022-12-02 18:35:43 +0530

    calculate wages

---
+++ EmployeeWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeWage
+    {
+        public static void CheckEmpWage()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random rand = new Random();
+            int empCheck = rand.Next(2);
+
+            if(empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("EmployeeWage" + empWage);
+        }
+
+    }
+}

2428bcd7fa3d72c9bcca9cd9de76b5018f93d027
gharjare <gharjarechetan@gmail.com>
2022-12-02 18:29:02 +0530

    Add wlcm msg

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeWageComputation
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+
+            // EmployeeCheck.CheckAttendence();
+            // EmployeeWage.CheckEmpWage();
+            // EmpPartTime.CheckPartTime();
+            //SwitchCase.CheckCases();
+            //  WagesForMonth.CalculateWageForMonth();
+          //  CalculateWagesInCondition.CheckCondition();
+        }
+    }
+}

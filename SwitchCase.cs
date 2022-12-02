88b65d25678aad70f68a1dc22b2d423a40d17e88
gharjare <gharjarechetan@gmail.com>
2022-12-02 18:40:00 +0530

    Add switch cases

---
+++ SwitchCase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class SwitchCase
+    {
+        public const int Is_Full_Time = 1;
+        public const int Is_Part_Time = 2;
+        public const int Emp_Rate_Per_Hour = 20;
+        public static void CheckCases()
+        {
+           
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            switch(empCheck)
+            {
+                case Is_Full_Time:
+                    Console.WriteLine("Employee is full time");
+                    empHrs = 8;
+                    break;
+
+                    case Is_Part_Time:
+                    Console.WriteLine("Employee is part time ");
+                    empHrs = 4;
+                    break;
+
+                    default:
+                    Console.WriteLine("Employee is absent");
+                    break;
+
+
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}

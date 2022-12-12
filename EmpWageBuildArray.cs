39686d8a39e59c8dc3da1f96c9e45e5398212e37
gharjare <gharjarechetan@gmail.com>
2022-12-12 16:59:43 +0530

    build array

---
+++ EmpWageBuildArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmpWageBuildArray
+    {
+        public const int Is_Full_Time = 1;
+        public const int Is_Part_Time = 2;
+
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWageArray;
+
+        public EmpWageBuildArray()
+        {
+            this.companyEmpWageArray = new CompanyEmpWage[5];
+        }
+
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays,int maxHoursPerMonth)
+        {
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numOfCompany++;
+        }
+
+        public void  computeEmpWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].setTotalEmpWage(this.computeEmpwage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].ToString());
+            }
+
+        }
+
+        public int computeEmpwage(CompanyEmpWage companyEmpWage)
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empcheck = random.Next(3);
+                switch (empcheck)
+                {
+                    case Is_Full_Time:
+                        Console.WriteLine("Employee is full time");
+                        empHrs = 8;
+                        break;
+                    case Is_Part_Time:
+                        Console.WriteLine("Employee is part time");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs = +empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + "Emp Hrs: " + empHrs);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+    }
+}
